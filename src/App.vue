a69f83ba9086e99acdd2cf1c68315d64647fbdc2
MatheusDLM <47402274+MatheusDLM@users.noreply.github.com>
2021-10-27 22:56:18 -0300

    Cronometro funcional

---
+++ src/App.vue
@@ -0,0 +1,181 @@
+<template>
+  <div id="app">
+    <img src="./assets/cronometro.png" class="img" />
+    <a class="timer"> {{numero}} </a>
+
+    <div class="areaBtn">
+      <button class="botao" @click="vai"> {{botao}} </button>
+      <button class="botao" @click="limpar">LIMPAR</button>
+    </div>
+
+    <div class="list" v-show="historico.length > 0">
+      <ul>
+        <li v-for="item in historico" :key="item">VOCÊ FEZ UMA PAUSA EM: {{item}}</li>
+      </ul>
+      <button @click="historico = []">Limpar historico</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      numero: 0,
+      botao: 'Começar',
+      timer: null,
+      ss:0,
+      mm: 0,
+      hh: 0,
+      historico: []
+    }
+  },
+  methods:{
+    vai(){
+
+      if(this.timer !== null){
+        //AQUI TEM ALGO RODANDO NO TIMER...
+        clearInterval(this.timer);
+        this.timer = null;
+        this.botao = 'Começar';
+        if(this.ss !== 0){
+          this.historico.push(this.numero);
+        }
+
+      }else{
+        //O TIMER ESTA ZERADO OU PARADO.
+
+        this.timer = setInterval(()=> {
+          this.rodarTimer();
+        }, 100); // 1 segundo = 1000 milisegundos
+        this.botao = 'PAUSAR';
+
+      }
+
+
+    },
+    limpar(){
+      if(this.timer !== null){
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+      this.ss = 0;
+      this.mm = 0;
+      this.hh = 0;
+      this.numero = 0;
+      this.botao = 'Começar';
+      this.historico = [];
+      
+    },
+    rodarTimer(){
+      this.ss++;
+
+      if(this.ss == 59) {
+        // Deu 59 segundos
+        this.ss = 0;
+        this.mm++;
+      }
+
+      if(this.mm == 59){
+        //Chegou a 59 minutos
+        this.mm = 0;
+        this.hh++;
+      }
+
+      let format = (this.hh < 10 ? '0'+this.hh : this.hh) + ':'
+      + (this.mm < 10 ? '0'+this.mm : this.mm) + ',' 
+      + (this.ss < 10 ? '0'+this.ss : this.ss);
+
+      return this.numero = format;
+
+    }
+  }
+
+}
+</script>
+
+<style>
+ #app{
+   display: flex;
+   width: 100%;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+ }
+
+ .img{
+   width: 420px;
+   height: 420px;
+   padding-top: 50px; 
+ }
+
+ .timer{
+   color: #ffffff;
+   font-size: 70px;
+   margin-top: -210px
+ }
+
+ .areaBtn{
+   margin-top: 155px;
+   display: flex;
+ }
+
+  .botao{
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    width: 150px;
+    background-color: #FFF;
+    font-size: 20px;
+    border:0;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: 15px;
+    margin-right: 15px;
+    padding: 6px;
+    cursor: pointer;
+  }
+
+  .botao:hover{
+    opacity: 0.8;
+    transition: all 0.50s;
+  }
+
+  ul{
+    text-align: center;
+    padding: 0px;
+  }
+
+  ul li{
+    margin-top: 4px;
+    padding: 15px;
+    background-color: rgb(70,70,70);
+    list-style: none;
+    color: #FFF;
+    font-size: 18px;
+    border-radius: 6px;
+  }
+
+  .list button{
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    width: 150px;
+    background-color: #FFF;
+    font-size: 15px;
+    border:0;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: 15px;
+    margin-right: 15px;
+    padding: 6px;
+    cursor: pointer;
+  }
+
+  .list button:hover{
+    opacity: 0.8;
+    transition: all 0.50s;
+  }
+
+</style>
